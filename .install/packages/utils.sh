be7a862a13eb1a5d459528d46331bb5f85c45bd2
caioax <acaioaugusto423@gmail.com>
2026-01-23 19:23:54 -0300

    refac(install): fix some files

--- .install/packages/utils.sh
+++ .install/packages/utils.sh
@@ -7,47 +7,50 @@
 
 UTILS_PACKAGES=(
     # Clipboard
-    "wl-clipboard"          # Wayland clipboard (wl-copy, wl-paste)
-    "cliphist"              # Clipboard history manager
+    "wl-clipboard" # Wayland clipboard (wl-copy, wl-paste)
+    "cliphist"     # Clipboard history manager
 
     # Media Control
-    "playerctl"             # Media player controller
+    "playerctl" # Media player controller
 
     # JSON Processing
-    "jq"                    # JSON processor (usado pelo workspace-manager)
+    "jq" # JSON processor (usado pelo workspace-manager)
 
     # Calculator
-    "bc"                    # Calculator (usado por scripts)
+    "bc" # Calculator (usado por scripts)
 
     # Notifications
-    "libnotify"             # Notification library (notify-send)
+    "libnotify" # Notification library (notify-send)
 
     # Network
-    "networkmanager"        # Network management
-    "network-manager-applet"  # NM applet (para quicksettings)
+    "networkmanager"         # Network management
+    "network-manager-applet" # NM applet (para quicksettings)
 
     # Bluetooth
-    "bluez"                 # Bluetooth stack
-    "bluez-utils"           # Bluetooth utilities
+    "bluez"       # Bluetooth stack
+    "bluez-utils" # Bluetooth utilities
 
     # Audio
-    "pipewire"              # Audio server
-    "pipewire-pulse"        # PulseAudio compatibility
-    "pipewire-alsa"         # ALSA compatibility
-    "wireplumber"           # Session manager
-    "pavucontrol"           # PulseAudio volume control
+    "pipewire"       # Audio server
+    "pipewire-pulse" # PulseAudio compatibility
+    "pipewire-alsa"  # ALSA compatibility
+    "wireplumber"    # Session manager
+    "pavucontrol"    # PulseAudio volume control
 
     # Brightness
-    "brightnessctl"         # Brightness control
+    "brightnessctl" # Brightness control
 
     # Screenshot
-    "grim"                  # Screenshot utility (usado pelo hyprshot)
-    "slurp"                 # Region selection (usado pelo hyprshot)
+    "grim"  # Screenshot utility (usado pelo hyprshot)
+    "slurp" # Region selection (usado pelo hyprshot)
 
     # Misc
-    "polkit-kde-agent"      # KDE Polkit agent
-    "qt5-wayland"           # Qt5 Wayland support
-    "qt6-wayland"           # Qt6 Wayland support
+    "polkit-kde-agent" # KDE Polkit agent
+    "qt5-wayland"      # Qt5 Wayland support
+    "qt6-wayland"      # Qt6 Wayland support
+
+    # Flatpak
+    "flatpak" # Flatpak
 )
 
 # Pacotes AUR
